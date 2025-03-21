5ba2b5d9e9cb721c3162d20940f67afc29d51b82
Dhruv <dhruv.gupta.cse17@iitbhu.ac.in>
2025-03-21 19:50:58 +0530

    added support for list of elements

--- app/main.py
+++ app/main.py
@@ -8,11 +8,84 @@ logging.basicConfig(level=logging.INFO)
 # import bencodepy - available if you need it!
 # import requests - available if you need it!
 
+
+def find_cur_element_end(elem_str):
+    # find next 'e' which indicates END of current element only.
+    e_idx = elem_str.find('e')
+    if e_idx == -1:
+        raise ValueError("Invalid encoded value")
+    return e_idx
+
+def decode_bencode(bencoded_value: bytes):
+    """
+
+    assumes list within list is not allowed.
+
+
+    :param bencoded_value:
+    :return:
+    """
+
+
+
+    result = []
+    is_list = False
+
+    # # get rid of the 'l' and ending 'e'
+    # s = bencoded_list_str[1:-1]
+    # cnt = 0
+    s = bencoded_value.decode()
+
+
+    while s:
+
+        match s:
+            case s if re.match(r'^\d', s):  # First character is a digit
+                colon_idx = s.find(':')
+                if colon_idx == -1:
+                    raise ValueError("Invalid encoded value")
+
+                length_of_elem = int(s[:colon_idx])
+                # eg: 12:sjdfhsldkfjghfd  => 2 + 1 = 3 is the content start idx.
+                content_start_idx = colon_idx + 1
+                elem_content = s[content_start_idx: content_start_idx+length_of_elem]
+                if len(elem_content) != length_of_elem:
+                    raise ValueError("Invalid encoded value")
+                result.append(elem_content)
+                # update s to next element
+                s = s[content_start_idx+length_of_elem:]
+
+            case s if s[0] == 'i':  # Starts with 'i'
+                end_idx = find_cur_element_end(s)
+                int_content = s[1:end_idx]
+                result.append(int(int_content))
+                # next element comes after the 'e'.
+                # update s to start from there.
+                s = s[end_idx+1:]
+
+            case s if s[0] == 'l': # Starts with 'l'
+                if s[-1] != 'e':
+                    raise ValueError("Invalid encoded value")
+                # we don't really need to do anything special for this
+                # so, simply update s.
+                # update s to ignore
+                s = s[1:-1]
+                is_list = True
+
+            case _:
+                raise ValueError("Invalid encoded value")
+
+
+    # if it has just one element, then it isn't really a list...
+    return result if is_list else result[0]
+
+
+
 # Examples:
 #
 # - decode_bencode(b"5:hello") -> b"hello"
 # - decode_bencode(b"10:hello12345") -> b"hello12345"
-def decode_bencode(bencoded_value: bytes):
+def decode_bencode_old(bencoded_value: bytes):
 
     result = ''
     s = bencoded_value.decode()
@@ -27,6 +100,10 @@ def decode_bencode(bencoded_value: bytes):
             logging.info("Starts with 'i' and ends with 'e'.")
             content = s[1:-1]
             result = int(content)
+        case s if re.match(r'^l.*e$', s): # Starts with 'l', ends with 'e'
+            logging.info("Starts with 'l' and ends with 'e'. (list)")
+
+
         case _:
             raise NotImplementedError("Only strings are supported at the moment")
 
@@ -57,8 +134,6 @@ def main():
             raise TypeError(f"Type not serializable: {type(data)}")
 
         # Uncomment this block to pass the first stage
-        decoded_val = decode_bencode(bencoded_value)
-        # print(decoded_val)
         print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
     else:
         raise NotImplementedError(f"Unknown command {command}")
--- app/tests/test_decode_bencode.py
+++ app/tests/test_decode_bencode.py
@@ -7,11 +7,7 @@ from app.main import decode_bencode
 def test_valid_string_decoding():
     assert decode_bencode(b"5:hello") == "hello"
     assert decode_bencode(b"11:hello world") == "hello world"
-    assert decode_bencode(b"20:http://abc.com/announce") == "http://abc.com/announce"
-
-def test_valid_int_decoding():
-    assert decode_bencode(b"i1401538053e") == 1401538053
-    assert decode_bencode(b"i-100e") == -100
+    assert decode_bencode(b"23:http://abc.com/announce") == "http://abc.com/announce"
 
 def test_invalid_string_format():
     with pytest.raises(ValueError, match="Invalid encoded value"):
@@ -19,8 +15,18 @@ def test_invalid_string_format():
     with pytest.raises(ValueError, match="Invalid encoded value"):
         decode_bencode(b"5:")
 
+def test_valid_int_decoding():
+    assert decode_bencode(b"i1401538053e") == 1401538053
+    assert decode_bencode(b"i-100e") == -100
+
+def test_valid_list_decoding():
+    assert decode_bencode(b"li1401538053ee") == [1401538053]
+    assert decode_bencode(b"l5:helloe") == ["hello"]
+    assert decode_bencode(b"l5:helloi1401538053e5:helloe") == ["hello",1401538053,"hello"]
+
+
 def test_unsupported_format():
-    with pytest.raises(NotImplementedError, match="Only strings are supported at the moment"):
+    with pytest.raises(ValueError, match="Invalid encoded value"):
         decode_bencode(b"bencoded")
-        with pytest.raises(NotImplementedError, match="Only strings are supported at the moment"):
-            decode_bencode(b"i123x")
+    with pytest.raises(ValueError, match="Invalid encoded value"):
+        decode_bencode(b"i123x")
